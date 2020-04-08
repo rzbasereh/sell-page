fa91a9da0153a86b0c23429247c070b02b8b875d
sadra <m.sad.ra.972@gmail.com>
2020-04-08 16:50:07 +0430

    driver created!

--- js/sell page.js
+++ js/sell page.js
@@ -1,18 +1,9 @@
 $(document).ready(function () {
-    $("div.col-9").mouseenter(function () {
-        $(this).css("cursor","pointer");
-    });
-    $("svg.bi-x").mouseenter(function () {
-        $(this).css("cursor", "pointer");
-    });
     $("i.fa-exchange-alt").click(function () {
         $("div.default").addClass('show');
     });
-    $("i.fa-exchange-alt").mouseenter(function () {
-        $(this).css("cursor", "pointer");
-    });
     $("#remove").click(function () {
-        $("div.default").remove('.show');
+        $("div.default").removeClass('show');
     });
     $("svg.bi-x").click(function () {
         $(this).closest("div.r1").remove()
